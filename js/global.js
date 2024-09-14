403f12fbdb00df0cba76fa43549ab2bb4d11a980
sebastianvasquezechavarria1234 <sebitas221314@gmail.com>
2024-09-14 10:39:55 -0500

    1 commit

---
+++ js/global.js
@@ -0,0 +1,11 @@
+// const header = document.querySelector("#header");
+
+// window.onscroll = function () {
+//     let scroll = document.documentElement.scrollTop;
+//     if(scroll > 10){
+//         header.style.backgroundColor = "white";
+//     }
+//     else if(scroll < 10){
+//         header.style.backgroundColor = "transparent";
+//     }
+// }
