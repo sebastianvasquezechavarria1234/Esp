475b4231c885d3b99aad0adf08f27f141f255e6e
sebastianvasquezechavarria1234 <sebitas221314@gmail.com>
2024-09-14 15:29:51 -0500

    responsive

--- js/global.js
+++ js/global.js
@@ -1,11 +1,30 @@
-// const header = document.querySelector("#header");
 
-// window.onscroll = function () {
-//     let scroll = document.documentElement.scrollTop;
-//     if(scroll > 10){
-//         header.style.backgroundColor = "white";
-//     }
-//     else if(scroll < 10){
-//         header.style.backgroundColor = "transparent";
-//     }
-// }
+
+const iconMenu = document.querySelector("#iconMenu");
+const iconX = document.querySelector("#iconX");
+const menuResponsive = document.querySelector(".menu__responsive");
+const menuResponsiveUl = document.querySelector(".menu__reponsive--ul");
+
+iconMenu.addEventListener("click", () => {
+    menuResponsive.classList.add("menuResponsiveActive")
+    menuResponsiveUl.classList.add("menuResponsiveUlActive")
+})
+
+iconX.addEventListener("click", () => {
+    menuResponsive.classList.remove("menuResponsiveActive")
+    menuResponsiveUl.classList.remove("menuResponsiveUlActive")
+})
+
+
+
+const botonTop = document.querySelector(".boton__top");
+
+window.onscroll = function () {
+    let scroll = document.documentElement.scrollTop;
+    if(scroll > 1000){
+        botonTop.classList.add("botonTopActive")
+    }
+    else if(scroll < 1000){
+        botonTop.classList.remove("botonTopActive")
+    }
+}
